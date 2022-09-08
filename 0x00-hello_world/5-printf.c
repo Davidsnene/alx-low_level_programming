8b604aac1b178b15bdee9332f749ca4ecefa28bf
davidsnene <davidsnene001@gmail.com>
2022-09-08 18:52:36 +0000

    add 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * printf - print word
  *
- * returne: the word
+ * returne: allways 0 (succes)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
