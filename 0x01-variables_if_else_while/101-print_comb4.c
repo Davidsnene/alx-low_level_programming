a948b0e037f35b7b1ed6e3ecf8640d09d3e5b7b1
davidsnene <davidsnene001@gmail.com>
2022-09-09 22:48:19 +0000

    add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations
+ * of three digits using putchar
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int y, x, z;
+
+	for (x = 48; x <= 55; x++)
+	{
+		for (y = 49; y <= 56; y++)
+		{
+			for (z = 50; z <= 57; z++)
+			{
+				if (z > y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					{
+					if (x != 55 || y != 56 || z != 57)
+					{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
