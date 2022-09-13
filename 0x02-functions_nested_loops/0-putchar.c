fb30eb515f980f1609fe44aee523d1928ef307c3
davidsnene <davidsnene001@gmail.com>
2022-09-13 22:28:39 +0000

    add 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: 'defining a header file that prints _putchar
+ * Description: print _putchar follow by new line
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-int main(void)
+int main (void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+        _putchar ("_");
+        _putchar ("p");
+        _putchar ("u");
+        _putchar ("t");
+        _putchar ("c");
+        _putchar ("h");
+        _putchar ("a");
+        _putchar ("r");
+        _putchar ("\n");
 
-	return (0);
+        return (0)
 }
