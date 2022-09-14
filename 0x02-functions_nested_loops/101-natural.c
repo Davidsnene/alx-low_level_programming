e0d25640c579ddc42488cb9a185c216f8ad52c71
davidsnene <davidsnene001@gmail.com>
2022-09-14 17:56:43 +0000

    add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry piont
+ *
+ * Description: sum of mutiples of 3 an multiple of 5
+ * under 1024
+ *
+ * Return: nothing
+ */
+int main (void);
+	int a;
+	int b;
+{
+	for (a = 0, a <= 1024, a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf ("%d, ", a)
+		}
+
+	}
+	
+	for (b = 0, b <= 1024, b++)
+	{
+		if (b % 5 == 0)
+		{
+			printf ("%d, ", b)
+		}
+	}
+
+	return (a + b);
+}
