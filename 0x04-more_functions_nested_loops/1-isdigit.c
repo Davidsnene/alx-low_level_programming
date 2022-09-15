16920588c770560987b722c553cdc1ff15c65f9d
davidsnene <davidsnene001@gmail.com>
2022-09-15 23:35:57 +0000

    add 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: variable to print
+ *
+ * Description: print 1 if digit otherwise 0
+ *
+ * Return: Nothing
+ */
+int _isdigit(int c)
+{
+	if (c => 48 && c <= 57)
+	{
+	return (1);
+	}
+return (0);
+}
