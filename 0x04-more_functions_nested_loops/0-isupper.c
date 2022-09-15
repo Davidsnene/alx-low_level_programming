ff64e037d1b2ad1794f369b1af0a7bb85d52a59c
davidsnene <davidsnene001@gmail.com>
2022-09-15 23:18:08 +0000

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * @c: Alphabet letter
+ *
+ * Description: prints 1 if upper letter otherwise 0
+ *
+ * Return: nothing
+ */
+int _isupper(int c)
+{
+	for (c = 65; c <= 90, c++)
+	{
+	return (1);
+	}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+For project 0x04. C - More functions, more nested loops
