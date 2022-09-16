6edbed6ad5159220b895c155b299963924859532
davidsnene <davidsnene001@gmail.com>
2022-09-16 00:46:04 +0000

    add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ *
+ * Description: draws line
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	while (a < n && n > 0)
+	{
+		_putchar('_');
+		a++;
+	}
+	_putchar('\n');
+}
