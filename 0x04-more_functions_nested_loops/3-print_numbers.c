6f5ac0a3dcc982cf3f0fb05670a8a3fb0054c667
davidsnene <davidsnene001@gmail.com>
2022-09-16 00:02:58 +0000

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: prints number follow by new line
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+		_putchar("\n");
+	}
+return (0);
+}
