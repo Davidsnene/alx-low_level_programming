cffe8faeb7ab83dc178ae3c7931f8634ca8a92e0
davidsnene <davidsnene001@gmail.com>
2022-09-16 01:05:43 +0000

    add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ * @size : integer type
+ *
+ * Description: prints a triangle, followed by a new line
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
