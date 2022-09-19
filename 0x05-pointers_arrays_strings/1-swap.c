fe7bc08dbc765d6b0d0ef3c84b3880ab2c46cb8f
davidsnene <davidsnene001@gmail.com>
2022-09-19 16:50:27 +0000

    add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - Entry point
+* @a: first integer
+* @b: second integer
+*
+* Description: swaps the values of two integers
+*
+* Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	a = 0;
+	b = 0;
+}
