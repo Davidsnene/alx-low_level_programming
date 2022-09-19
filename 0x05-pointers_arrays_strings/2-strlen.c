326c4161e42162eaf08dce51e6afd2068a09bfb6
davidsnene <davidsnene001@gmail.com>
2022-09-19 23:04:25 +0000

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strlen - Entry point
+* @*s: pointer
+* @s: the tring
+*
+* Description: retuns the lenght of a string
+*
+* Return: Nothing
+*/
+
+void _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	i++;
+
+	return (i);
+}
