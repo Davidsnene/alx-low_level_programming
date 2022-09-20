bdda1d15565bb4995bc9ae91ea1c3d53c95a991a
davidsnene <davidsnene001@gmail.com>
2022-09-20 00:47:34 +0000

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - Entry point
+* @s: string to print
+*
+* Description: prints string in inverse
+*
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int x;
+
+	x = 0
+	while (s[x]);
+	x++;
+
+	while (x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
