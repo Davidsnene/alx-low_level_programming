a44dda0ef754294c5dec16adfff1161976504663
davidsnene <davidsnene001@gmail.com>
2022-09-21 11:54:05 +0000

    add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strcat - Entry point
+* @dest: first string
+* @src: second string
+*
+* Description: concatenates two strings
+*
+* Return: a char
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	dest = NULL;
+	src = NULL;
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+For project 0x06. C - More pointers, arrays and strings
