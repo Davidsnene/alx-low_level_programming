8454a4d43a8cff4248b77a68f949954520e08a27
davidsnene <davidsnene001@gmail.com>
2022-09-21 19:11:04 +0000

    add 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	*dest = 0;
-	*src = 0;
-return (0);
+	int i, j;
+
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	return (dest);
 }
