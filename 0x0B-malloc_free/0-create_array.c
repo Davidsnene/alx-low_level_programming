b99436913b9aab95b3cb2d0576996ada044ea898
davidsnene <davidsnene001@gmail.com>
2022-10-03 19:46:15 +0000

    add 0-creat_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_ar - Entry point
+* @size: The size of the array to be initialized.
+* @c: The specific char to intialize the array with.
+*
+* Description:
+*
+* Return: Nothing
+*/
+char *create_ar(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ar = malloc(sizeof(char) * size);
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+
+	return (ar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+for project 0x0B. C - mallo, free
